2cb24b53c7e5c2068d4682d30e5a5e099eb297d8
Freddie Ridell <DrVonTrap@gmail.com>
2016-10-04 11:18:04 +0000

    feat(bbpl): bitbucket does full modern deploy

--- Scripts/deploy-dev.sh
+++ Scripts/deploy-dev.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-shopt -s globstar
-
-s3cmd \
-	sync \
-	--access_key=$AWS_ACCESS_KEY_ID \
-	--secret_key=$AWS_SECRET_ACCESS_KEY \
-	--exclude-from .s3ignore \
+aws s3 sync \
+	--exclude "$(cat .s3ignore)" \
 	_site/ \
 	s3://codogo-watership-down-site-dev/$BITBUCKET_BRANCH/
-
-
-for CSS in _site/**/*.css
-do
-	s3cmd \
-		sync \
-		--access_key=$AWS_ACCESS_KEY_ID \
-		--secret_key=$AWS_SECRET_ACCESS_KEY \
-		--exclude-from .s3ignore \
-		-m text/css \
-		$CSS \
-		s3://codogo-watership-down-site-dev/$BITBUCKET_BRANCH/$(echo $CSS | sed -e "s/_site\///") 
-done
--- Scripts/deploy-master.sh
+++ Scripts/deploy-master.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-shopt -s globstar
-
-s3cmd \
-	sync \
-	--access_key=$AWS_ACCESS_KEY_ID \
-	--secret_key=$AWS_SECRET_ACCESS_KEY \
-	--delete-removed \
-	--exclude-from .s3ignore \
+aws s3 sync \
+	--exclude "$(cat .s3ignore)" \
+	--delete \
 	_site/ \
 	s3://codogo-watership-down-site-prod/
 
-for CSS in _site/**/*.css
-do
-	s3cmd \
-		sync \
-		--access_key=$AWS_ACCESS_KEY_ID \
-		--secret_key=$AWS_SECRET_ACCESS_KEY \
-		--exclude-from .s3ignore \
-		-m text/css \
-		$CSS \
-		s3://codogo-watership-down-site-prod/$(echo $CSS | sed -e "s/_site\///") 
-done
+aws configure set preview.cloudfront true
+
+aws cloudfront create-invalidation \
+	--distribution-id ENY36WYXVXRL4 \
+	--paths "/*"
